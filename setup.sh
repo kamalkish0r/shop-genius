8bdf58a580a28230ed21d59ec69bbb7e4aef56d8
kamalkish0r <ikkamalkish0r@gmail.com>
2023-10-23 18:44:01 +0530

    fixed setup script

--- setup.sh
+++ setup.sh
@@ -16,16 +16,23 @@ else
     source env\Scripts\activate
 fi
 
-# Step 3: Create a .env file and ask the user to set OPENAI_API_KEY
-echo "Please set your OPENAI_API_KEY in the .env file."
-echo "You can generate the API key from the OpenAI website at this link: https://platform.openai.com/signup"
-
-# Step 4: Install project requirements
+# Step 3: Install project requirements
 pip install -r requirements.txt
 
-# Step 5: Install Playwright Dependencies
+# Step 4: Install Playwright Dependencies
 playwright install
 
-echo "Project setup is complete."
 
-# Optionally, you can provide further instructions or start your project here.
+# Step 5: Create a .env file and ask the user to set OPENAI_API_KEY
+echo -e "\n\n\nNow let's your OPENAI_API_KEY in the .env file."
+echo "You can generate the API key from the OpenAI website at this link: https://platform.openai.com/account/api-keys"
+touch .env
+
+# Ask the user to enter the OPENAI_API_KEY
+read -p "Please enter your OPENAI_API_KEY: " OPENAI_API_KEY
+
+# Write the OPENAI_API_KEY to the .env file
+echo "OPENAI_API_KEY=$OPENAI_API_KEY" >> .env
+
+echo "Project setup is complete!!"
+echo -e "\nYou can starting shopping buy running the bot.py"
